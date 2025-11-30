9f67285eb7ee0d0c4aa7b4b881be74726cf6dad2
David Ansa <davitacols@gmail.com>
2025-11-30 19:27:48 +0100

    sleek

--- app/api/nearby-buildings/route.ts
+++ app/api/nearby-buildings/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/lib/prisma-client';
+import { prisma } from '@/lib/prisma-client';
 
 export async function GET(request: NextRequest) {
   try {
